ddd58a21a109576dde168a69f3403bfe88af4f7b
tolik19bat <play.hoffa@gmail.com>
2023-11-30 17:48:58 +0300

    open_commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,77 @@
 // TODO: write your code here
-import sum from './basic';
+// import sum from './basic';
+//
+// console.log('worked');
+//
+// console.log(sum([1, 2]));
+//=====================================================
 
-console.log('worked');
+const character = [{
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон'
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...'
+      // <- обратите внимание, описание "засекречено"
+    }
+  ]
+},
+   {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон'
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...'
+        // <- обратите внимание, описание "засекречено"
+      }
+    ]
+  }]
+// console.log(character)
 
-console.log(sum([1, 2]));
+
+class Team {
+  constructor() {
+      this.members = new Set();
+  }
+  add(argument) {
+    if(this.members) {
+      throw new Error('character already exists');
+    }
+    this.members.add(argument);
+  }
+  addAll(...rest) {//как реализовать проверку массива и удалить дубль объект
+  this.members.add(rest);
+    console.log(this.members)
+  }
+  toArray() {
+
+  }
+}
+const result = new Team();
+result.addAll(character);
+result.add(character);
+console.log(result);
