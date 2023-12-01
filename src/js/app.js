6ce4f150cfdc5e3293b7672eb10cc9c7ec15a6d2
tolik19bat <play.hoffa@gmail.com>
2023-12-01 18:26:46 +0300

    open_commit

--- src/js/app.js
+++ src/js/app.js
@@ -6,72 +6,64 @@
 // console.log(sum([1, 2]));
 //=====================================================
 
-const character = [{
-  name: 'Лучник',
-  type: 'Bowman',
-  health: 50,
-  level: 3,
-  attack: 40,
-  defence: 10,
-  special: [
-    {
-      id: 8,
-      name: 'Двойной выстрел',
-      icon: 'http://...',
-      description: 'Двойной выстрел наносит двойной урон'
-    },
-    {
-      id: 9,
-      name: 'Нокаутирующий удар',
-      icon: 'http://...'
-      // <- обратите внимание, описание "засекречено"
-    }
-  ]
-},
-   {
-    name: 'Лучник',
-    type: 'Bowman',
-    health: 50,
-    level: 3,
-    attack: 40,
-    defence: 10,
-    special: [
-      {
-        id: 8,
-        name: 'Двойной выстрел',
-        icon: 'http://...',
-        description: 'Двойной выстрел наносит двойной урон'
-      },
-      {
-        id: 9,
-        name: 'Нокаутирующий удар',
-        icon: 'http://...'
-        // <- обратите внимание, описание "засекречено"
-      }
-    ]
-  }]
+//
+// const character = [{
+//   name: 'Лучник',
+//   name: 'Лучник',//задвоение свойства и значения
+//   type: 'Bowman',
+//   health: 50,
+//   level: 3,
+//   attack: 40,
+//   defence: 10,
+//   special: [
+//     {
+//       id: 8,
+//       name: 'Двойной выстрел',
+//       icon: 'http://...',
+//       description: 'Двойной выстрел наносит двойной урон'
+//     },
+//     {
+//       id: 9,
+//       name: 'Нокаутирующий удар',
+//       icon: 'http://...'
+//       // <- обратите внимание, описание "засекречено"
+//     }
+//   ]
+// }]
 // console.log(character)
 
 
-class Team {
-  constructor() {
-      this.members = new Set();
+export default class Team {                 //создаём класс
+  constructor() {            //конструктор
+    this.members = new Set();//присваеваем объект set
   }
-  add(argument) {
-    if(this.members) {
-      throw new Error('character already exists');
-    }
-    this.members.add(argument);
-  }
-  addAll(...rest) {//как реализовать проверку массива и удалить дубль объект
-  this.members.add(rest);
-    console.log(this.members)
-  }
-  toArray() {
 
+  add(argument) {                                 //метод добавления карточки игрока
+    if (this.members) {                           //если карточка присутствует в переменной
+      throw new Error('character already exists');//выводим ошибку
+    } else {                                      //если отсутствует
+    this.members.add(argument);                   //добовляем новую
+    }
+    // console.log(this.members)
+  }
+
+  addAll(...rest) {                    // метод добавления коллекции карточек
+    this.members.add(...new Set(rest));//добавляем коллекцию без задвоений
+    // console.log(this.members)
+  }
+
+  toArray(character) {           //метод конвертации объекта character в массив
+    let array = [];              //создаём новый массив
+    for (let item of character) {//методом перебора
+      // console.log(item)
+      array = Object.entries(item)//присваеваем массив собственных перечисляемых свойств
+    }
+    // console.log(array)
+    return array;                 //возвращаем новый массив
   }
 }
-const result = new Team();
-result.addAll(character);
-result.add(character);
-console.log(result);
+
+// const result = new Team();
+// result.add(character);
+// result.addAll(character);
+// result.toArray(character)
