b3da8232d779b2c324c6fb757c06b2ff33e32695
tolik19bat <play.hoffa@gmail.com>
2023-12-02 22:03:22 +0300

    new_commit

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -5,3 +5,87 @@ test('should sum', () => {
 
   expect(result).toBe(6);
 });
+//===================================================
+import Team from '../app';
+
+//входные данные
+const character = [{
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон'
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...'
+      // <- обратите внимание, описание "засекречено"
+    }
+  ]
+}];
+
+const character1 = [{
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон'
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...'
+      // <- обратите внимание, описание "засекречено"
+    }
+  ]
+}];
+
+const team = new Team();
+
+test('add()добавляет персонажа в команду', () => {
+  team.add(character);
+  expect(team.members.size).toBe(1);
+  // expect([...team.members]).toContain(character);
+});
+
+test('add() должен выдать ошибку, если член уже существует', () => {
+  team.add(character);
+  expect(() => team.add(character)).toThrow('character already exists');
+});
+
+test('addAll() следует добавить в команду несколько участников без дубликатов', () => {
+  team.addAll(character);
+  expect([...team.members]).toBe(character1);
+});
+
+test('toArray() должен преобразовать членов команды в массив записей', () => {
+
+  const result = team.toArray(character);
+
+ const arr = [
+  [ 'name', 'Лучник' ],
+  [ 'type', 'Bowman' ],
+  [ 'health', 50 ],
+  [ 'level', 3 ],
+  [ 'attack', 40 ],
+  [ 'defence', 10 ],
+  [ 'special', [ [Object], [Object] ] ]
+];
+
+  expect(result).toEqual(arr);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,34 @@
 // TODO: write your code here
-// import sum from './basic';
-//
-// console.log('worked');
-//
-// console.log(sum([1, 2]));
+import sum from './basic';
+
+console.log('worked');
+
+console.log(sum([1, 2]));
 //=====================================================
 
-//
-// const character = [{
-//   name: 'Лучник',
-//   name: 'Лучник',//задвоение свойства и значения
-//   type: 'Bowman',
-//   health: 50,
-//   level: 3,
-//   attack: 40,
-//   defence: 10,
-//   special: [
-//     {
-//       id: 8,
-//       name: 'Двойной выстрел',
-//       icon: 'http://...',
-//       description: 'Двойной выстрел наносит двойной урон'
-//     },
-//     {
-//       id: 9,
-//       name: 'Нокаутирующий удар',
-//       icon: 'http://...'
-//       // <- обратите внимание, описание "засекречено"
-//     }
-//   ]
-// }]
+
+const character = [{
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон'
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...'
+      // <- обратите внимание, описание "засекречено"
+    }
+  ]
+}];
 // console.log(character)
 
 
@@ -44,26 +43,26 @@ export default class Team {                 //создаём класс
     } else {                                      //если отсутствует
     this.members.add(argument);                   //добовляем новую
     }
+    console.log(this.members)
+  }
+
+  addAll(...argument) {                    // метод добавления коллекции карточек
+    this.members.add(...new Set(argument));//добавляем коллекцию без задвоений
     // console.log(this.members)
   }
 
-  addAll(...rest) {                    // метод добавления коллекции карточек
-    this.members.add(...new Set(rest));//добавляем коллекцию без задвоений
-    // console.log(this.members)
-  }
-
-  toArray(character) {           //метод конвертации объекта character в массив
+  toArray(argument) {           //метод конвертации объекта character в массив
     let array = [];              //создаём новый массив
-    for (let item of character) {//методом перебора
+    for (let item of argument) {//методом перебора
       // console.log(item)
       array = Object.entries(item)//присваеваем массив собственных перечисляемых свойств
     }
-    // console.log(array)
+    console.log(array)
     return array;                 //возвращаем новый массив
   }
 }
 
-// const result = new Team();
+const result = new Team();
 // result.add(character);
 // result.addAll(character);
-// result.toArray(character)
+result.toArray(character)
